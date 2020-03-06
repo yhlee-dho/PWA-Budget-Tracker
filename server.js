64523d62c979932717a5ccfeeadb432c1cac2c88
unknown <yhlee89.dho@gmail.com>
2020-03-06 01:13:40 -0600

    fixed style.css to styles.css in service-worker, moved all files out of dev folder

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const mongojs = require("mongojs");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5432;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -17,7 +18,18 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const databaseURL = process.env.MONGODB_URI || "PWA-BudgetTracker";
+const collections = ["transactions"]
+
+const db = mongojs(databaseURL, collections)
+
+db.on("error", error => {
+  console.log(`db error: ${error}`)
 });
 
 // routes here
